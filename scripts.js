b40e86e408efec04a31c93183edf33fdded49492
liamshatzel <liam.shatzel@gmail.com>
2023-01-21 18:37:57 -0800

    soundcloud widget

--- scripts.js
+++ scripts.js
@@ -5,10 +5,9 @@ function inputChange() {
     $("#start").prop("disabled", true);
 
     let mins = 00;
-    let secs = 00;
 
     mins = $("#mins-input").val();
-    secs = $("#secs-input").val();
+    console.log(mins);
 
     var countDownDate = new Date().getTime();
 
@@ -16,8 +15,6 @@ function inputChange() {
     countDownDate = addMinutes(countDownDate, mins);
 
     //add seconds to countdown date
-    countDownDate = addSeconds(countDownDate, secs);
-    const origTime = countDownDate;
 
     //with jquery
     myfunc = setInterval(function () {
@@ -71,4 +68,75 @@ function addMinutes(date, minutes) {
 function addSeconds(date, seconds) {
     return new Date(date + seconds * 1000);
 }
+
+
+var iframeElement = document.querySelector('iframe');
+var iframeElementID = iframeElement.id;
+var widget = SC.Widget(iframeElement);
+var x = document.getElementById("play");
+
+
+
+
+widget.bind(SC.Widget.Events.FINISH, function () {
+    widget.getCurrentSound(function (currentSound) {
+        document.getElementById("currentTrack").innerHTML = currentSound.title;
+    });
+});
+
+function play() {
+    if (x.innerHTML === "▶") {
+        x.innerHTML = "||";
+    } else {
+        x.innerHTML = "▶";
+    }
+    widget.toggle();
+};
+
+function next() {
+    x.innerHTML = "||";
+    widget.next();
+    widget.seekTo(0);
+    widget.getCurrentSound(function (currentSound) {
+        document.getElementById("currentTrack").innerHTML = currentSound.title;
+    });
+};
+
+function prev() {
+    x.innerHTML = "||";
+    widget.prev();
+    widget.seekTo(0);
+    widget.getCurrentSound(function (currentSound) {
+        document.getElementById("currentTrack").innerHTML = currentSound.title;
+    });
+};
+
+widget.bind(SC.Widget.Events.READY, function () {
+    widget.getCurrentSound(function (currentSound) {
+        document.getElementById("currentTrack").innerHTML = currentSound.title;
+
+        widget.getSounds(function (tracks) {
+            for (var i in tracks) {
+                if (tracks[i].title != undefined) {
+                    $('#tracklist').append("<li class='track-item' id='" + i + "'" + ">" + tracks[i].title + "</li>");
+
+                }
+            }
+
+            $(".track-item").click(function () {
+                var s = this.id
+                widget.seekTo(0);
+                widget.skip(s);
+                x.innerHTML = "||"; widget.getCurrentSound(function (currentSound) {
+                    document.getElementById("currentTrack").innerHTML = currentSound.title;
+                });
+
+            });
+
+
+
+        });
+
+    });
+});
 
